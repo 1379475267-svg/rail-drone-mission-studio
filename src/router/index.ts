ff9e45a073ee4c46647a9156d676c1f2d6938b77
Haoran Fei <1379475267@qq.com>
2026-07-24 21:08:33 +0800

    Use hash history for Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+
+const history = import.meta.env.MODE === 'pages'
+  ? createWebHashHistory(import.meta.env.BASE_URL)
+  : createWebHistory(import.meta.env.BASE_URL)
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history,
   routes: [
     {
       path: '/',
